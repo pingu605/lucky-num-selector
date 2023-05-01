41ab28d93775ce6377873dab73ba9d0b0bcd70d4
pingu605 <pingu6052@gmail.com>
2023-05-01 14:43:29 +0900

    feat: make def(get_lucky_nums)

--- selector.py
+++ selector.py
@@ -1,6 +1,11 @@
-from random import sample
-
-result = random.sample(range(1,45+1),k=6)
-print(result)
+import random
 
 
+def get_lucky_nums():
+    return random.sample(range(1,45+1), k=6)
+
+
+if __name__ == '__main__':
+    times = int(input('Enter num(1-100): '))
+    for _ in range(times):
+        print(get_lucky_nums())
